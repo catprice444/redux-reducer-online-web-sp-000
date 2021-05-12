e56a3b5b8dd538220d67faee171a6ea5715dc39e
catprice444 <catprice444@gmail.com>
2021-05-12 07:19:56 -0400

    commit tree 8a3e010fc7a9f57fa2a6ba80ba8bc22208c76715

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,45 @@
-export function manageFriends(state, action){
+// export function manageFriends(state = {friends: []}, action){
+//     switch(action.type){
+//         case "ADD FRIEND":
+//             return{
+//                 ...state, 
+//                 friends: [
+//                     ...state.friends, 
+//                     action.friend
+//                 ]
+//             }
+//         case "REMOVE FRIEND":
+//             let removeFriend = state.friends.findIndex(friend => friend.id === action.id)
+//             return{
+//                 ...state, 
+//                 friends: [
+//                     ...state.friends.slice(0, removeFriend),
+//                     ...state.friends.slice(removeFriend + 1)
+//                 ]
+//             }
+//         default:
+//             return state; 
+//     }
+// }
+
+export function manageFriends(state = {friends: []} , action){
+    switch(action.type){
+        case 'ADD_FRIEND':
+        // console.log(action)
+        return {
+            ...state, friends: [
+                ...state.friends, action.friend
+            ]
+        }
+        case 'REMOVE_FRIEND':
+        const removeFriend = state.friends.findIndex(friend => friend.id === action.id)
+        return ({
+            ...state, friends : [
+                ...state.friends.slice(0, removeFriend),
+                ...state.friends.slice(removeFriend + 1)
+            ]
+        })
+        default:
+            return state;
+    }
 }
